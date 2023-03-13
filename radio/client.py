515a43005812d1f1fe6069bb29f3128cfac4c504
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2023-03-13 15:43:22 +0900

    Update client.py

--- radio/client.py
+++ radio/client.py
@@ -1,6 +1,8 @@
 ##############################################################################
 # queueを1にした時にradio.receice_full()でmsg、rssiとtimestampの値を取得できるか？ #
 # 下記のコードからmsg、rssiとtimestampを合わせて一つのqueueとするということがわかった。#
+# radio.receive()、radio.receive_byte()かradio.receive_full()することでqueueを #
+# 1つ受け取る。queueを受け取らないと新たに受け取ったメッセージが破棄される              #
 ##############################################################################
 
 from microbit import *
@@ -10,9 +12,9 @@ radio.config(queue=1, group=1)
 radio.on()
 
 while True:
-    details = radio.receive_full()
-    if details:
-        msg, rssi, timestamp = details
-        print("message:"+str(msg))
-        print("rssi:"+str(rssi))
-        print("timestamp:"+str(timestamp))
+	details = radio.receive_full()
+	if details:
+		msg, rssi, timestamp = details
+		print("message:"+str(msg))
+		print("rssi:"+str(rssi))
+		print("timestamp:"+str(timestamp))
