91db49e772525695c8eba7d63a5b3758ffbe906b
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2023-03-14 10:39:23 +0900

    Update transmitter.py

--- radio/transmitter.py
+++ radio/transmitter.py
@@ -1,16 +1,16 @@
 from microbit import *
 import radio
 
-display.show(Image.HEART)
+display.show(Image.ASLEEP)
 
 radio.config(group=1)
 radio.on()
 
 i = 0
 while True:
-	display.show(Image.HEART)
-	sleep(500)
-	radio.send(str(i))
-	i += 1
-	display.show(Image.YES)
-	sleep(500)
+    display.show(Image.YES)
+    sleep(500)
+    radio.send(str(i))
+    i += 1
+    display.show(Image.ASLEEP)
+    sleep(500)
