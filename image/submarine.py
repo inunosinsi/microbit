edec997660d788ac171d3ea5aa4d56a5b0900482
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2023-03-15 06:14:29 +0900

    Create submarine.py

---
+++ image/submarine.py
@@ -0,0 +1,9 @@
+from microbit import *
+
+submarine = Image("99000:"
+                 "09000:"
+                 "09909:"
+                 "99999:"
+                 "99999:")
+
+display.show(submarine)
