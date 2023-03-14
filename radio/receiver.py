e00019ac63756f9cdcfa8dda436b4ba249e8d765
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2023-03-14 10:42:23 +0900

    Update receiver.py

--- radio/receiver.py
+++ radio/receiver.py
@@ -1,14 +1,14 @@
 from microbit import *
 import radio
 
-display.show(Image.HEART)
+display.show(Image.TARGET)
 
 radio.config(group=1)
 radio.on()
 
 i = 0
 while True:
-	if button_a.is_pressed():
-		msg = radio.receive()
-		print(msg)
-		sleep(300)
+    if button_a.is_pressed():
+        msg = radio.receive()
+        print(msg)
+        sleep(300)
