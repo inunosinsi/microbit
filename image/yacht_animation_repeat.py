518f2cd269c672e582a330131ab1d72e55d98b35
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2023-03-15 15:05:42 +0900

    Update yacht_animation_repeat.py

--- image/yacht_animation_repeat.py
+++ image/yacht_animation_repeat.py
@@ -1,11 +1,11 @@
 from microbit import *
 
 mat = [
-    [0,0,9,0,0,0,0,0,0,0],
-    [0,0,9,9,9,0,0,0,0,0],
-    [0,0,9,0,0,0,0,0,0,0],
-    [9,9,9,9,9,0,0,0,0,0],
-    [0,9,9,9,0,0,0,0,0,0]
+    [0,0,9,0,0,0,0,0,0,0,0],
+    [0,0,9,9,9,0,0,0,0,0,0],
+    [0,0,9,0,0,0,0,0,0,0,0],
+    [9,9,9,9,9,0,0,0,0,0,0],
+    [0,9,9,9,0,0,0,0,0,0,0]
 ]
 
 icons = []
@@ -19,14 +19,15 @@ while True:
             if i > 4:
                 break
         coord += ":"
-    
-    yacht = Image(coord)
-    icons.append(yacht)
 
+    # メモリ対策でアイコンをすぐに出力する
+    yacht = Image(coord)
+    display.show(yacht)
+
+    sleep(1000)
+    
     for row in range(0, len(mat)):
-        mat[row][len(mat[0])] = mat[row][0]
+        mat[row][len(mat[0])-1] = mat[row][0]
         for col in range(0, len(mat[0])-1):
             mat[row][col] = mat[row][col+1]
         
-
-display.show(icons, delay=1000)
