f5c090b219d8474f5cbfdd785d1dce6332d8f2fd
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2023-03-15 12:31:07 +0900

    Create yacht_matrix.py

---
+++ image/yacht_matrix.py
@@ -0,0 +1,18 @@
+from microbit import *
+
+mat = [
+    [0,0,9,0,0],
+    [0,0,9,9,9],
+    [0,0,9,0,0],
+    [9,9,9,9,9],
+    [0,9,9,9,0]
+]
+
+coord = ""
+for vec in mat:
+	for v in vec:
+		coord += str(v)
+	coord += ":"
+
+yacht = Image(coord)
+display.show(yacht)
