6ffc8ce4664bc94ad964a16845778a5bc184ad02
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2023-03-15 12:48:31 +0900

    Update yacht_animation.py

--- image/yacht_animation.py
+++ image/yacht_animation.py
@@ -8,6 +8,7 @@ mat = [
     [0,9,9,9,0]
 ]
 
+icons = []
 for i in range(0, 6):
     coord = ""
     for vec in mat:
@@ -16,11 +17,11 @@ for i in range(0, 6):
         coord += ":"
 
     yacht = Image(coord)
-    display.show(yacht)
-
-    sleep(1000)
+    icons.append(yacht)
 
     for row in range(0, len(mat)):
         for col in range(0, len(mat[0])-1):
             mat[row][col] = mat[row][col+1]
         mat[row][len(mat[0])-1] = 0
+
+display.show(icons, delay=1000)
