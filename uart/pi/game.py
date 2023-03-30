4cc3d0a5a9ec0ca89f8e997608a6db931a2d70d0
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2023-03-30 13:26:49 +0900

    Update game.py

--- uart/pi/game.py
+++ uart/pi/game.py
@@ -39,5 +39,5 @@ while True:
         
     for event in pygame.event.get():
         if event.type == QUIT:
-                pygame.quit()
-                sys.exit()
+            pygame.quit()
+            sys.exit()
