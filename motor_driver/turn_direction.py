7f78f6132ca1acf68750d76938cb8857c3ae0ba0
Tsuyoshi Saito <tsuyoshi@saitodev.co>
2023-07-06 12:59:25 +0900

    Update turn_direction.py

--- motor_driver/turn_direction.py
+++ motor_driver/turn_direction.py
@@ -1,10 +1,11 @@
 # Imports go at the top
 from microbit import *
 
+# Code in a 'while True:' loop repeats forever
 while True:
     if button_a.is_pressed():
-        pin12.write_analog(512)
+        pin12.write_digital(1)
         pin8.write_digital(0)
     elif button_b.is_pressed():
-        pin8.write_analog(512)
         pin12.write_digital(0)
+        pin8.write_digital(1)
